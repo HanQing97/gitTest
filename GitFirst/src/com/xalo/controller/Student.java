5ce6cd53d7269293da000de1a7049bdb9219d8d3
Admin <Admin@DESKTOP-L7KJJDU>
2019-08-16 11:19:09 +0800

    sb

---
+++ GitFirst/src/com/xalo/controller/Student.java
@@ -0,0 +1,11 @@
+package com.xalo.controller;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÁõÍØ´óÉµ±Æ");
+
+	}
+
+}
